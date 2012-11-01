2193ba240acacc1b4bf8143c46fb28437392e368
Artem Karpenko <gooyozh@gmail.com>
2012-11-01 11:52:36 +0200

    Some more snippets, CSS transforms

--- src/main/scala/org/yozh/snippet/TestSnippet.scala
+++ src/main/scala/org/yozh/snippet/TestSnippet.scala
@@ -1,6 +1,8 @@
 package org.yozh.snippet
 
 import xml.NodeSeq
+import java.util.Date
+import net.liftweb.util.BindHelpers._
 
 /**
  *
@@ -9,5 +11,13 @@ import xml.NodeSeq
 class TestSnippet {
   def render(in: NodeSeq): NodeSeq = <span>Test snippet render</span>
 
-  def renderMethod(in: NodeSeq): NodeSeq = <span>Test render via method</span>
+  def renderMethod(in: NodeSeq): NodeSeq = <span>Test render via method: {TestObject.balance}</span>
+
+  def changeInput = "#balance *" #> TestObject.balance & "#date *+" #> TestObject.date.toString
+}
+
+object TestObject {
+  def balance = 42
+
+  def date = new Date
 }
